a6220438059c6bd87528adee962fb43ff2c08d83
sultanasafia <147336363+sultanasafia@users.noreply.github.com>
2023-12-05 09:38:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+// Q1====================
+// let number = 3.45214;
+// document.write("number " +number + "<br>")
+// let roundof = Math.round(number);
+// document.write("roundof "+ roundof+ "<br>")
+// let floor = Math.floor(number);
+// document.write("floor" +floor + "<br>")
+
+// let ceil = Math.ceil(number);
+// document.write("ceil " +ceil)
+// Q2=======================
+// let number = -2.673;
+// document.write("number " +number + "<br>")
+// let roundof = Math.round(number);
+// document.write("roundof "+ roundof+ "<br>")
+// let floor = Math.floor(number);
+// document.write("floor" +floor + "<br>")
+
+// let ceil = Math.ceil(number);
+// document.write("ceil " +ceil)
+// Q3=======================
+
+// let number = -4;
+// let ans = Math.abs(number)
+// console.log(`The absolute value of ${number} is ${ans}`);
+
+// Q4 =======================
+
+    // let diceValue =Math.floor( Math.random()*6) +1
+    // console.log(diceValue)
+// Q5 =======================
+
+//   let toss = Math.floor(Math.random()*2)+1
+//   let ans = toss < 2 ? "Head":"Tail";
+//    document.write(`<p>The coin shows: ${ans}</p>`);
+ 
+// Q6 =======================
+// let toss = Math.floor(Math.random()*100)+1
+// console.log(toss)
+// Q7 =======================
+
+    // let userInput = prompt("Please enter your weight (e.g., 50, 50kgs, 50.2kgs, 50.2kilograms)");
+    // let weight = parseFloat(userInput)
+    // if(!isNaN(weight)){
+    //   document.write(`Your weight is: ${weight} kgs`);
+    // } else {
+    //   document.write(`Invalid input. Please enter a valid weight.`);
+    // }
+  // Q8 =======================
+
+// let secretNumber = Math.floor(Math.random() * 10) + 1;
+// let userInput = +prompt("Guess the secret number between 1 and 10:");
+// if (!isNaN(userInput) && userInput >= 1 && userInput <= 10) {
+//   if (userInput === secretNumber) {
+//     console.log("Congratulations! You guessed the secret number!");
+//   } else {
+//     console.log(`Sorry, the secret number was ${secretNumber}. Try again!`);
+//   }
+// } else {
+//   console.log("Please enter a valid number between 1 and 10.");
+// }
+
+// ==================== Assignment completed ============
